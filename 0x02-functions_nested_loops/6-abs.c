c15789a036478313cbd6b57bc1ef5be0038ad0b5
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-09-13 12:08:00 +0300

    absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs -> return absolute value of a number
+* @n: parameter
+* Return: absolute value of a number
+*/
+int _abs(int j)
+{
+	if (j < 0)
+	{
+		j = -j;
+	}
+	return (j);
+}
