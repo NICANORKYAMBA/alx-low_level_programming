eccc686b0da9848f0e94608b175701eea31ba58e
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-09-13 11:17:18 +0300

    check whether a character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* function to check whether a character is an alphabet
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
+}
