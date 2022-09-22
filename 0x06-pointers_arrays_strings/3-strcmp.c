6b39dca7a40b17bdaf1c21e6c5fe3db6c4041e2b
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-09-22 10:25:37 +0300

    stringcompare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
-		i++;
-	return (*(s1 + i) - *(s2 + i));
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
