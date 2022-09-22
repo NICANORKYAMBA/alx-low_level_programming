7844bf8ddf9319e01b1c88f8e8420b9ca3e15554
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-09-22 13:15:57 +0300

    capitalizes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,9 +7,7 @@
  */
 char *cap_string(char *str)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (str[i])
 	{
@@ -29,7 +27,7 @@ char *cap_string(char *str)
 		    str[i - 1] == ')' ||
 		    str[i - 1] == '{' ||
 		    str[i - 1] == '}' ||
-		    i == 0)
+		    i== 0)
 			str[i] -= 32;
 
 		i++;
