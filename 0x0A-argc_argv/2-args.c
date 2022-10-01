b68b0f08d24d815c5568f54c1009aab05fc7beb5
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-10-01 20:54:17 +0300

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+/**
+ * main - a program that prints all arguments it receives.
+ *
+ * @argc: the number of arguments
+ * @argv:arguments passed to the program
+ * Return: int
+ */
 int main(int argc, char **argv)
-{	
+{
 	int i;
 
 	for (i = 0; i < argc; i++)
