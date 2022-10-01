3ea4cc8b7d7447522a7cb30753758ec11f524f80
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-10-01 20:38:05 +0300

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 /**
-* main - print the name of the program
-* @argc: Count arguments
-* @argv: Arguments
-*
-* Return: Always 0 (Success)
-*/
-int main(int argc, char *argv[])
+ * main - a program that prints the number of arguments passed into it.
+ *
+ * @argc: number of arguments
+ * @argv: arguments
+ * @__attribute__((unused)): unused variable
+ * Return: int
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
 {
-(void) argv; /*Ignore argv*/
-printf("%i\n", argc - 1);
-return (0);
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
 }
