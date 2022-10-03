5771f91898707a8570ec9e15e4fd328f676796ad
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-10-03 12:38:58 +0300

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
+	char *ptr;
 	int i, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
@@ -24,16 +24,16 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
-	concat_str = malloc(sizeof(char) * len);
+	ptr = malloc(sizeof(char) * len);
 
-	if (concat_str == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
-		concat_str[concat_index++] = s1[i];
+		ptr[concat_index++] = s1[i];
 
 	for (i = 0; s2[i]; i++)
-		concat_str[concat_index++] = s2[i];
+		ptr[concat_index++] = s2[i];
 
-	return (concat_str);
+	return (ptr);
 }
