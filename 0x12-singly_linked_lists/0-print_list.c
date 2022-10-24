a89828405a8f99582bcd9b4f7a8a86f941a92788
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-10-24 15:37:47 +0300

    fixing print all elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,23 +6,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t n;
+	size_t nodes = 0;
 
-	n = 0;
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
 
-		n++;
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
 	}
-	return (n);
+
+	return (nodes);
 }
