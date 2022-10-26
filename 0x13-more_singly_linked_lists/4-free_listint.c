e81fd02b86027af2083490911048a591f15527b9
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-10-26 13:13:31 +0300

    free listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list.
+ *
+ * @head: pointer to listint_t list
+ */
+void free_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		if (head->next != NULL)
+		{
+			free_listint(head->next);
+		}
+		free(head);
+	}
+}
