3c7583af67e5bb47723313983011eef5be25e647
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-11-02 17:40:39 +0300

    copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,9 @@
 
 #include <stddef.h>
 
+#define ERROR "Error: Can't read from file %s\n"
+#define ERR "Error: Can't write to %s\n"
+
 int _putchar(char c);
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
