8d1ea7d2444725390606ec06637ffbad56492df2
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2022-12-08 15:10:40 +0300

    freeing a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *free_dlistint - frees the list.
+ *@head: pointer to the head node.
+ *
+ *Return: Nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+	free(tmp);
+}
